f3b2c0e921f84e85ffc6d26a41b82bcd6c6ac7a4
Alfredo Martínez <xshaffter@gmail.com>
2022-10-22 21:28:49 -0600

    feat(k8s): services auto-assign their ip

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+import os
 from typing import Dict
 
 from common.fastapi.core.parameters import get_param_manager
@@ -9,36 +10,26 @@ from kubernetes.client import AppsV1Api, CoreV1Api, Configuration, ApiClient
 
 class Config(ParameterManager):
     is_simulation: bool = Definition(True)
-    k8s_api_key: Dict = Environ(dict())
-    k8s_api_key_prefix: Dict = Environ(dict())
-    k8s_host: str = Environ("http://localhost:443")
-    k8s_password: str = Environ(None)
-    k8s_username: str = Environ(None)
+    k8s_config_file: str = Environ(...)
 
     @staticmethod
     def get_k8s_config():
-        params = get_param_manager()
-        configuration = Configuration(
-            host=params.variables.k8s_host,
-            api_key=params.variables.k8s_api_key,
-            password=params.variables.k8s_password,
-            username=params.variables.k8s_username,
-            api_key_prefix=params.variables.k8s_api_key_prefix,
-            discard_unknown_keys=False
+        config.load_kube_config(
+            config_file=get_param_manager().variables.k8s_config_file,
         )
 
-        return configuration
+        params = get_param_manager()
 
     @staticmethod
     def get_k8_client() -> AppsV1Api:
-        configuration = Config.get_k8s_config()
-        api_client = ApiClient(configuration=configuration)
+        Config.get_k8s_config()
+        api_client = ApiClient()
         k8s_client = client.AppsV1Api(api_client)
         return k8s_client
 
     @staticmethod
     def get_k8s_core_api() -> CoreV1Api:
-        configuration = Config.get_k8s_config()
-        api_client = ApiClient(configuration=configuration)
+        Config.get_k8s_config()
+        api_client = ApiClient()
         k8s_client = client.CoreV1Api(api_client)
         return k8s_client
--- src/routers/kube.py
+++ src/routers/kube.py
@@ -1,12 +1,14 @@
 import os
+import random
 from datetime import datetime
+from time import sleep
 
 import yaml
 from common.fastapi.db import CRUDDal, get_dal_dependency, get_dal
 from common.fastapi.routing import post, BaseRouter, delete
 from common.fastapi.schemas import HTTP_201_CREATED, HTTPResponseModel, HTTP_200_REMOVED
 from fastapi import Path, Depends, Body
-from kubernetes.client import V1Service
+from kubernetes.client import V1Service, V1ServiceList
 
 from src import Config
 from src.db.models.enums.modality import Modality
@@ -22,8 +24,8 @@ class KubeRouter(BaseRouter):
                       service_dal: CRUDDal[Service] = Depends(get_dal_dependency(CRUDDal, Service))):
 
         service_dal._auto_commit = False
-
-        service_count = service_dal.list(service_type=service_type.name, is_active=True).count() + 1
+        used_ports = [service.used_port for service in service_dal.list(service_type=service_type.name, is_active=True)]
+        service_count = service_dal.get_object_query(service_type=service_type.name, is_active=True).count() + 1
         service_middlescore = service_type.name.replace("_", "-")
         service_name = f"{service_middlescore}-{service_count}"
 
@@ -43,12 +45,17 @@ class KubeRouter(BaseRouter):
                 .replace("{{service-name}}", service_name) \
                 .replace("{{world-volume}}", world.world_path)
 
-        with open('base_kube_yamls/node-port.yaml') as mine_yaml:
-            service_file_content = "".join(mine_yaml.readlines()).replace("{{service-name}}", service_name)
+        with open('base_kube_yamls/load-balancer.yaml') as mine_yaml:
+            rnd_port = random.randint(40000, 50000)
+            while rnd_port in used_ports:
+                rnd_port = random.randint(40000, 50000)
+            service_file_content = "".join(mine_yaml.readlines()).replace("{{service-name}}", service_name)\
+                .replace("{{service-port}}", f"{rnd_port}")
 
-        new_yaml = f'yamls/{service_middlescore}-{service_count}.yaml'
-        new_yaml_fw = f'yamls/{service_middlescore}-{service_count}-fw.yaml'
+        new_yaml = f'yamls/{service_name}.yaml'
+        new_yaml_fw = f'yamls/{service_name}-fw.yaml'
 
+        host = None
         with open(new_yaml, "w+") as deployment_yaml:
             deployment_yaml.write(file_content)
 
@@ -59,24 +66,41 @@ class KubeRouter(BaseRouter):
             service_instance = yaml.safe_load(service_yaml)
             client = Config.get_k8s_core_api()
             response: V1Service = client.create_namespaced_service(body=service_instance, namespace="default")
-            port = response.spec.ports[0].node_port
+            port = response.spec.ports[0].port
 
         with open(new_yaml) as deployment_yaml:
             instance = yaml.safe_load(deployment_yaml)
             client_app = Config.get_k8_client()
             client_app.create_namespaced_deployment(body=instance, namespace="default")
 
+        client = Config.get_k8s_core_api()
+        search = True
+        while search:
+            services: V1ServiceList = client.list_namespaced_service(namespace="default")
+            for serv in services.items:  # type: V1Service
+                if serv.metadata.name == f"{service_name}-server":
+                    try:
+                        host = serv.status.load_balancer.ingress[0].ip
+                        search = False
+                    except IndexError:
+                        host = None
+                    except TypeError:
+                        host = None
+                    break
+            sleep(10)
+
         service_dal.create(dict(
             service_path=new_yaml,
             service_type=service_type.name,
             service_name=service_name,
             started_at=datetime.now(),
-            used_port=port
+            used_port=f"{host}:{port}"
         ))
         service_dal.commit()
+
         return HTTPResponseModel(status_code=200, detail=dict(
             service_name=service_name,
-            node_port=port
+            url_path=f"{host}:{port}"
         ))
 
     @delete('/service/stop/{service_name}', response_model=HTTPResponseModel)
@@ -86,6 +110,8 @@ class KubeRouter(BaseRouter):
         client = Config.get_k8s_core_api()
         client_app = Config.get_k8_client()
         try:
+            os.remove(f"/app/yamls/{service_name}-fw.yaml")
+            os.remove(f"/app/yamls/{service_name}.yaml")
             os.remove(service.service_path)
         except:
             pass
@@ -96,7 +122,7 @@ class KubeRouter(BaseRouter):
         ), service_name=service_name, is_active=True)
 
         try:
-            client.delete_namespaced_service(f"{service_name}-fw", namespace="default")
+            client.delete_namespaced_service(f"{service_name}-server", namespace="default")
             client_app.delete_namespaced_deployment(f"{service_name}-deployment", namespace="default")
         except:
             return HTTPResponseModel(status_code=200, detail=dict(
